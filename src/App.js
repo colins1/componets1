12bc533938d092d2ef37bae97dbe5f303fde466b
colins1 <maxrozhkov1@gmail.com>
2023-02-12 00:07:31 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import './App.css';
 import './bootstrap.css';
-
+import React from 'react';
+import List from './component/List';
+import ListItem from './component/ListItem';
 function App() {
   return (
     <div className="App">
-      <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="..."/>
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
+      <List>{(item) => <ListItem key={item}>{item}</ListItem>}</List>
     </div>
   );
 }
